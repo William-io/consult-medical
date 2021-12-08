fc68c462c1f674700bb41ad563daafc1ccc2bc11
William-io <capuletos@live.com>
2021-12-07 23:01:05 -0300

    User logged in and fixed

--- ConsultApp/src/app/app.component.ts
+++ ConsultApp/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
 
+export class AppComponent implements OnInit {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+  ) {
+  }
+  ngOnInit(): void {
+    this.afAuth.onAuthStateChanged((data: any) => {
+      if (data) {
+        this.router.navigateByUrl('/');
+      } else {
+        this.router.navigateByUrl('/login');
+      }
+    });
+    this.afAuth.idToken.subscribe(token => console.log(token));
+  }
 }
+
--- ConsultApp/src/app/pages/login/login.component.ts
+++ ConsultApp/src/app/pages/login/login.component.ts
@@ -18,6 +18,7 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
   }
 
   login() {
